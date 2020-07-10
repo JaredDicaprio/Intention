2383d7c38dfb150d4b9d36403d586fb483b5f130
Paul D. Duncan <chesiredarling@gmail.com>
2020-07-09 17:22:32 -0700

    initial

---
+++ next.config.js
@@ -0,0 +1,9 @@
+// must restart server whenever you make changes in next.config
+module.exports = {
+  env: {
+    MONGO_SRV: "mongodb+srv-url",
+    JWT_SECRET: "hjdfhuiklnlkbfcagdahsakjsdjdqw",
+    CLOUDINARY_URL: "https://api.cloudinary.com/v1_1-url",
+    STRIPE_SECRET_KEY: "stripe-secret-key"
+  }
+};
