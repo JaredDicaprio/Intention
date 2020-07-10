1808df7916ad419cb38dd31e7557b628aec0fc82
Paul D. Duncan <chesiredarling@gmail.com>
2020-07-09 17:25:53 -0700

    intention added

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === 'production'
-    ? 'https://grounded.vercel.app'
+    ? 'https://intention.vercel.app'
     : 'http://localhost:3000';
 
 export default baseUrl;
